60f5f4feb1f0a669a27f032be24f3d3deb9ba05e
Shayan <xenos_pr@yahoo.com>
2024-04-04 15:39:01 +0330

    Made the first draft of stamp removal function. Works, but leaves duplicate lines because it doesn't check for when a timestamp starts and finishes.

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import re
 
 
-def timeStampRemover(str: srt):
+def timeStampRemover(srt : str):
 
-    timestamp = re.compile(r"(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})")
-    pass
+    #timestampPattern = re.compile(r"(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})")
+    timestampPattern = re.compile(r'\d+\n\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}')
+    return re.sub(timestampPattern, " ", srt)
 
 
 def srt2txt(srt_path, txt_path):
     """
     A file type or address is used here to turn a srt file into a txt file.
     """
-    pass
+    with open(srt_path, "r") as srt_file:
+        with open(txt_path, "w") as txt_file:
+            stampRemoved = timeStampRemover(srt_file.read())
+            txt_file.write(stampRemoved)
+
 def main():
-    pass
+    srt2txt("./Bake Faster with Blender 2.80 [rrUWC6vKQaQ].en.srt", "./Bake Faster with Blender 2.80 [rrUWC6vKQaQ].en.txt")
+
     
 
-if name == "__main__":
+if __name__ == "__main__":
     main()

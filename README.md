eee7889a0027867d97f68756642aee601e871795
Shayan <xenos_pr@yahoo.com>
2024-04-04 15:19:23 +0330

    Made the initial naming and making of files.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# SRT2TXT
+# A simple package to remove SRT file's timestamps and save them into a text file
---
+++ main.py
@@ -0,0 +1,20 @@
+import re
+
+
+def timeStampRemover(str: srt):
+
+    timestamp = re.compile(r"(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})")
+    pass
+
+
+def srt2txt(srt_path, txt_path):
+    """
+    A file type or address is used here to turn a srt file into a txt file.
+    """
+    pass
+def main():
+    pass
+    
+
+if name == "__main__":
+    main()

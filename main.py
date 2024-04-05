2231be58cf4f782ddb3a43b76e170a81c7c029ea
Shayan <xenos_pr@yahoo.com>
2024-04-05 08:21:51 +0330

    Made a dirty tweek to be able to avoid duplicates. Needs more work.

--- main.py
+++ main.py
@@ -15,7 +15,29 @@ def srt2txt(srt_path, txt_path):
     with open(srt_path, "r") as srt_file:
         with open(txt_path, "w") as txt_file:
             stampRemoved = timeStampRemover(srt_file.read())
-            txt_file.write(stampRemoved)
+            pruned = ""
+            
+            for i, line in enumerate(stampRemoved.splitlines()):
+                print(line)
+                for j in range(i+1, i + min(10, len(stampRemoved.splitlines())-1 -i)):
+                    if line == stampRemoved.splitlines()[j] :
+                        print(line)
+                        line = "\n"
+                pruned = pruned + line
+            final = "" 
+            for line in pruned.splitlines():
+                if line =="\n" or line =="" or line == " ":
+                    pass
+                else:
+                    final += line + "\n"
+                    
+            # stampRemoved = pruned
+            # for i, line in enumerate(stampRemoved.splitlines()):
+            #     if line == "\n" and stampRemoved.splitlines()[max(i + 1, len(stampRemoved))-1] == "\n":
+            #         line = "*********"
+
+            #     pruned = pruned + line
+            txt_file.write(final)
 
 def main():
     srt2txt("./Bake Faster with Blender 2.80 [rrUWC6vKQaQ].en.srt", "./Bake Faster with Blender 2.80 [rrUWC6vKQaQ].en.txt")
